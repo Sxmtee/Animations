9b56a352efbc6951dbaada196b9731ad2b49fa4b
sxmtee <somtoo76@gmail.com>
2024-06-06 13:40:50 +0100

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by mac on 06/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
